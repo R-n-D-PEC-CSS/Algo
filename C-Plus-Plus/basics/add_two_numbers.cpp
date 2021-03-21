afd1d8c18d94db311fa27989e765fa5b2eb13c3c
D <dajayk12@gmail.com>
2021-03-21 21:56:51 +0530

    Created cpp algo to add two numbers

---
+++ C-Plus-Plus/basics/add_two_numbers.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << 'Enter two numbers\n';
+
+    cin >> num1 >> num2;
+
+    cout << 'Sum is = ' << num1+num2;
+
+    return 0;
+
+    /*
+        Enter two numbers
+        3 5
+    */
+
+    /*
+        Sum is = 8
+    */
+}
